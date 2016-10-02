dd9325c615a6b81cab81f047460d2569c10cf809
cushhelfrich <cushhelfrich@hotmail.com>
2016-10-02 00:23:28 -0400

    FirstCommit-cushhelfrich

---
+++ src/githubtesting/GitHubTesting.java
@@ -0,0 +1,20 @@
+package githubtesting;
+
+/** 
+ * @Course: SDEV 250 ~ Java Programming I
+ * @Author Name: Cush
+ * @Assignment Name: githubtesting
+ * @Date: Oct 2, 2016
+ * @Description: 
+ */
+//Imports
+
+//Begin Class GitHubTesting
+public class GitHubTesting {
+
+    //Begin Main Method
+    public static void main(String[] args) {
+
+    } //End Main Method
+
+} //End Class GitHubTesting
