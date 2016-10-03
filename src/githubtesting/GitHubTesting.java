0c612559e79482ba63365153e718cc0ebd722933
cushhelfrich <cushhelfrich@hotmail.com>
2016-10-02 22:06:41 -0400

    I changed something 3-cush

--- src/githubtesting/GitHubTesting.java
+++ src/githubtesting/GitHubTesting.java
@@ -17,6 +17,7 @@ public class GitHubTesting {
         System.out.println("Test clone push");
         System.out.println("Testing again");
         System.out.println("Third test");
+        jlj
     } //End Main Method
 
 } //End Class GitHubTesting
